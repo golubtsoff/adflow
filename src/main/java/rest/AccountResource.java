ecf429cc72fa1b4a2a55ca84384aa4a72b6379d8
Евгений Голубцов <evigol@mail.ru>
2019-06-10 20:37:51 +0700

    refactored AccountResource

--- src/main/java/rest/AccountResource.java
+++ src/main/java/rest/AccountResource.java
@@ -1,35 +1,41 @@
-package rest.admin;
+package rest;
 
 import com.google.gson.Gson;
 import entity.users.Account;
-import entity.users.Status;
-import entity.users.user.Contact;
-import entity.users.user.Person;
 import entity.users.user.Role;
-import entity.users.user.User;
-import exception.DbException;
+import entity.users.user.UserToken;
 import exception.NotFoundException;
-import rest.Roles;
 import rest.users.autentication.Secured;
 import service.AccountService;
-import service.UserService;
 import util.JsonHelper;
 
 import javax.persistence.OptimisticLockException;
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.HttpHeaders;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.math.BigDecimal;
 
-@Path("/admin/users")
 @Secured
-@Roles(Role.ADMIN)
 public class AccountResource {
 
+    @Context
+    HttpHeaders headers;
+
     @GET
-    @Path("{uid}/account")
+    @Path("/client/account")
+    @Roles({Role.CUSTOMER, Role.PARTNER})
     @Produces(MediaType.APPLICATION_JSON)
-    public Response read(@PathParam("uid") long userId){
+    public Response read(){
+        try {
+            long userId = Long.valueOf(headers.getHeaderString(UserToken.UID));
+            return getResponseRead(userId);
+        } catch (Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private Response getResponseRead(long userId){
         try {
             Account account = AccountService.get(userId);
             if (account == null)
@@ -41,10 +47,20 @@ public class AccountResource {
     }
 
     @PUT
-    @Path("{uid}/account")
+    @Path("/client/account")
+    @Roles({Role.CUSTOMER, Role.PARTNER})
     @Consumes(MediaType.APPLICATION_JSON)
     @Produces(MediaType.APPLICATION_JSON)
-    public Response update(@PathParam("uid") long userId, String content){
+    public Response update(String content){
+        try{
+            long userId = Long.valueOf(headers.getHeaderString(UserToken.UID));
+            return getResponseUpdate(userId, content);
+        } catch (Exception e){
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
+    }
+
+    private Response getResponseUpdate(long userId, String content){
         try{
             Gson gson = JsonHelper.getGson();
             Account accountFromClient = gson.fromJson(content, Account.class);
@@ -58,6 +74,23 @@ public class AccountResource {
         } catch (Exception e){
             return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
         }
+    }
+
+    @GET
+    @Path("/admin/client/{uid}/account")
+    @Roles(Role.ADMIN)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response read(@PathParam("uid") long userId){
+        return getResponseRead(userId);
+    }
+
+    @PUT
+    @Path("/admin/client/{uid}/account")
+    @Roles(Role.ADMIN)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response update(@PathParam("uid") long userId, String content){
+        return getResponseUpdate(userId, content);
     }
 
 }

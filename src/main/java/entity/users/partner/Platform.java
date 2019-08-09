6565e839a47f30cdff4fdda5cb2f3e94a0643964
Евгений Голубцов <evigol@mail.ru>
2019-08-09 13:04:59 +0700

    added creation and removed date for platform

--- src/main/java/entity/users/partner/Platform.java
+++ src/main/java/entity/users/partner/Platform.java
@@ -8,6 +8,7 @@ import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -20,6 +21,8 @@ public class Platform {
     public static final String DESCRIPTION = "DESCRIPTION";
     public static final String CPM_RATE = "CPM_RATE";
     public static final String PICTURE_FORMAT_ID = "PICTURE_FORMAT_ID";
+    public static final String CREATION_DATE = "CREATION_DATE";
+    public static final String REMOVED_DATE = "REMOVED_DATE";
     public static final String ACTION = "ACTION";
     public static final String STATUS = "STATUS";
 
@@ -47,6 +50,12 @@ public class Platform {
     @JoinColumn(name = PICTURE_FORMAT_ID)
     private PictureFormat pictureFormat;
 
+    @Column(name = CREATION_DATE)
+    private LocalDateTime creationDate;
+
+    @Column(name = REMOVED_DATE)
+    private LocalDateTime removedDate;
+
     @Column(name = ACTION)
     @Enumerated(EnumType.STRING)
     private Action action;
@@ -59,21 +68,57 @@ public class Platform {
     }
 
     public Platform(Partner partner) {
-        this(null, partner, null, null, null);
+        this(null, partner, null, null, null, null);
     }
 
-    public Platform(Partner partner, String title, String description, BigDecimal cpmRate) {
-        this(null, partner, title, description, cpmRate);
+    public Platform(
+            Partner partner,
+            String title,
+            String description,
+            BigDecimal cpmRate,
+            PictureFormat pictureFormat) {
+        this(null, partner, title, description, cpmRate, pictureFormat);
     }
 
-    public Platform(Long id, Partner partner, String title, String description, BigDecimal cpmRate) {
+    public Platform(
+            Long id,
+            Partner partner,
+            String title,
+            String description,
+            BigDecimal cpmRate,
+            PictureFormat pictureFormat) {
+        this(null, partner, title, description, cpmRate, pictureFormat, Action.STOP, Status.CHECKING);
+    }
+
+    public Platform(
+            Partner partner,
+            String title,
+            String description,
+            BigDecimal cpmRate,
+            PictureFormat pictureFormat,
+            Action action,
+            Status status) {
+        this(null, partner, title, description, cpmRate, pictureFormat, action, status);
+    }
+
+    public Platform(
+            Long id,
+            Partner partner,
+            String title,
+            String description,
+            BigDecimal cpmRate,
+            PictureFormat pictureFormat,
+            Action action,
+            Status status) {
         this.id = id;
         this.partner = partner;
         this.title = title;
         this.description = description;
         this.cpmRate = cpmRate;
-        this.status = Status.CHECKING;
-        this.action = Action.STOP;
+        this.pictureFormat = pictureFormat;
+        this.creationDate = LocalDateTime.now();
+        this.status = status;
+        this.action = action;
     }
 
     public Long getId() {
@@ -134,6 +179,26 @@ public class Platform {
 
     public void setStatus(Status status) {
         this.status = status;
+        if (status == Status.REMOVED){
+            this.setRemovedDate(LocalDateTime.now());
+            this.setAction(Action.STOP);
+        }
+    }
+
+    public LocalDateTime getCreationDate() {
+        return creationDate;
+    }
+
+    public void setCreationDate(LocalDateTime creationDate) {
+        this.creationDate = creationDate;
+    }
+
+    public LocalDateTime getRemovedDate() {
+        return removedDate;
+    }
+
+    public void setRemovedDate(LocalDateTime removedDate) {
+        this.removedDate = removedDate;
     }
 
     @Override
@@ -145,6 +210,8 @@ public class Platform {
                 ", description='" + description + '\'' +
                 ", cpmRate=" + cpmRate +
                 ", pictureFormat=" + pictureFormat +
+                ", creationDate=" + creationDate +
+                ", removedDate=" + removedDate +
                 ", action=" + action +
                 ", status=" + status +
                 '}';
@@ -154,14 +221,31 @@ public class Platform {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (!(o instanceof Platform)) return false;
-        Platform that = (Platform) o;
-        return Objects.equals(getId(), that.getId()) &&
-                Objects.equals(getTitle(), that.getTitle());
+        Platform platform = (Platform) o;
+        return Objects.equals(getId(), platform.getId()) &&
+                Objects.equals(getPartner(), platform.getPartner()) &&
+                Objects.equals(getTitle(), platform.getTitle()) &&
+                Objects.equals(getDescription(), platform.getDescription()) &&
+                Objects.equals(getCpmRate(), platform.getCpmRate()) &&
+                Objects.equals(getPictureFormat(), platform.getPictureFormat()) &&
+                Objects.equals(getCreationDate(), platform.getCreationDate()) &&
+                Objects.equals(getRemovedDate(), platform.getRemovedDate()) &&
+                getAction() == platform.getAction() &&
+                getStatus() == platform.getStatus();
     }
 
     @Override
     public int hashCode() {
-
-        return Objects.hash(getId(), getTitle());
+        return Objects.hash(
+                getId(),
+                getPartner(),
+                getTitle(),
+                getDescription(),
+                getCpmRate(),
+                getPictureFormat(),
+                getCreationDate(),
+                getRemovedDate(),
+                getAction(),
+                getStatus());
     }
 }
